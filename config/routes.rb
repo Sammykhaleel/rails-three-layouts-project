d2cf06a8bd3a0ceac5aacf819df648ee7dfc7f0a
sammy khaleel <sammykhaleel83@gmail.com>
2018-05-27 00:21:17 -0400

    customized show routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,15 @@
 Rails.application.routes.draw do
-  resources :portfolios
+##override the default routes
+  resources :portfolios, except: [:show]
+  get 'portfolio/:id', to: 'portfolios#show', as: 'portfolio_show'
+####### 
+
   root 'pages#home'
 
   # get 'pages/about'
   get 'about', to: 'pages#about'
-  get 'contact', to: 'pages#contact'
-
   # get 'pages/contact'
+  get 'contact', to: 'pages#contact'
 
   resources :blogs
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
