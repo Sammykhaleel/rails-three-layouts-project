485412a90125c6048862da96918eb89c190feaad
sammy khaleel <sammykhaleel83@gmail.com>
2018-06-07 16:00:17 -0400

    implemented imgaes default value in the portfolios

--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -1,3 +1,21 @@
 class Portfolio < ApplicationRecord
 	validates_presence_of :title, :body, :main_image, :thumb_image
+
+	def self.angular
+		where(subtitle: "Angular")
+	end
+
+	#second way of scopping
+	scope :ruby_on_rails_portfolio_items, -> {where(subtitle: 'Ruby on Rails')}
+
+	after_initialize :set_defaults
+
+	def set_defaults
+		# if self.main_image = nil
+		# 	self.main_image = "http://via.placeholder.com/600x400"
+		# end
+		#Another way of writing it.
+		self.main_image ||= "http://via.placeholder.com/600x400"
+		self.thumb_image ||= "http://via.placeholder.com/350x200"
+	end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
 ##override the default routes
   resources :portfolios, except: [:show]
+  get 'angular-items', to: 'portfolios#angular'
   get 'portfolio/:id', to: 'portfolios#show', as: 'portfolio_show'
 ####### 
 
